6f41b7069d0ca7616502d215dea0c36b78fb676e
Sebastian Fastner <s.fastner@sebastian-software.de>
2017-11-23 21:56:36 +0100

    Add execution functions

--- src/common/io.js
+++ src/common/io.js
@@ -1,3 +1,18 @@
+import { spawn } from "child_process"
+import Promise from "bluebird"
+
+export function exec(command, ...parameter) {
+  return new Promise((resolve) => { // eslint-disable-line
+    const proc = spawn(command, parameter, {
+      stdio: "inherit"
+    })
+
+    proc.on("close", (code) => {
+      resolve(code)
+    })
+  })
+}
+
 export function execNpm(command, ...parameter) {
-  console.log("RUN", "NPM", command, parameter)
+  return exec("npm", command, ...parameter)
 }
