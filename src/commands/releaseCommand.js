85adcc32877b8d7d855a01d43720547afc32cb8e
Sebastian Fastner <s.fastner@sebastian-software.de>
2017-12-01 00:23:49 +0100

    Add release boilerplate

--- src/commands/releaseCommand.js
+++ src/commands/releaseCommand.js
@@ -1,3 +1,22 @@
+import chalk from "chalk"
+import {
+  configurationAvailable,
+  getConfiguration,
+  printConfigurationErrors
+} from "../common/configuration"
+import initCommand from "./initCommand"
+
 export default async function releaseCommand(context) {
+  if (!await configurationAvailable()) {
+    return await initCommand(context)
+  }
+
+  const configuration = await getConfiguration()
+  if (configuration.hasErrors) {
+    console.log(chalk.red("wolke configuration has errors"))
+    return printConfigurationErrors(configuration)
+  }
+
   console.log("RELEASE", context)
+  return 0
 }
