dc9a539aafacae1d63d57178d5e1c76a19144803
Sebastian Fastner <s.fastner@sebastian-software.de>
2017-12-01 00:24:37 +0100

    Add cert command to check for valid TLS certs

--- src/commands/certCommand.js
+++ src/commands/certCommand.js
@@ -1,3 +1,65 @@
+import chalk from "chalk"
+import ora from "ora"
+
+import {
+  configurationAvailable,
+  getConfiguration,
+  printConfigurationErrors
+} from "../common/configuration"
+import {
+  getCertIdForDomain,
+  getWildcardCertIdForDomain,
+  requestNewCert
+} from "../common/cert"
+import initCommand from "./initCommand"
+
 export default async function certCommand(context) {
-  console.log("CERT", context)
+  if (!await configurationAvailable()) {
+    return await initCommand(context)
+  }
+
+  const configuration = await getConfiguration()
+  if (configuration.hasErrors) {
+    console.log(chalk.red("wolke configuration has errors"))
+    return printConfigurationErrors(configuration)
+  }
+
+  const spinner = ora("Check if certificates are valid").start()
+  const devDomain = configuration.value("developmentDomain")
+  const prodDomain = configuration.value("productionDomain")
+
+  let [ devCertId, prodCertId ] = await Promise.all([
+    getWildcardCertIdForDomain(devDomain),
+    getCertIdForDomain(prodDomain)
+  ])
+
+  if (!devCertId && !prodCertId) {
+    spinner.fail(
+      `No certificate for ${devDomain} (development statge)
+       and ${prodDomain} (production stage) assigned`
+    )
+  } else if (!devCertId) {
+    spinner.fail(`No certificate for ${devDomain} (development statge) assigned`)
+  } else if (!prodCertId) {
+    spinner.fail(`No certificate for  ${prodDomain} (production stage) assigned`)
+  } else {
+    spinner.succeed("All certificates correctly assigned")
+  }
+
+  if (!devCertId) {
+    devCertId = await requestNewCert(devDomain, {
+      wildcard: true
+    })
+  }
+
+  if (!prodCertId) {
+    prodCertId = await requestNewCert(prodDomain)
+  }
+
+  if (context.flags.verbose) {
+    console.log(`${chalk.cyan("Development certificate: ")} ${chalk.green(devCertId)}`)
+    console.log(`${chalk.cyan("Production certificate: ")} ${chalk.green(prodCertId)}`)
+  }
+
+  return 0
 }
