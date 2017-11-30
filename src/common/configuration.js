93c5d579f95804c46ec5ab015343eec1665b30a2
Sebastian Fastner <s.fastner@sebastian-software.de>
2017-12-01 00:22:29 +0100

    Add processing of configuration file

--- src/common/configuration.js
+++ src/common/configuration.js
@@ -1,5 +1,40 @@
-import { appPkg } from "./appPackage"
+import chalk from "chalk"
+import { appPkg, ROOT } from "./appPackage"
+import get from "lodash/get"
+import Promise from "bluebird"
+import filesystem from "fs"
+import path from "path"
+
+const readFile = Promise.promisify(filesystem.readFile) // eslint-disable-line
 
 export async function configurationAvailable() {
   return "wolke" in appPkg
+}
+
+export async function getConfiguration() {
+  return {
+    value: (key) => get(appPkg, `wolke.${key}`),
+    hasErrors: false,
+    errors: []
+  }
+}
+
+export function printConfigurationErrors(configuration) {
+  configuration.errors.forEach((element) => {
+    console.log(chalk.red(`- ${element}`))
+  })
+
+  return 1
+}
+
+export async function getClaudiaConfig(key) {
+  try {
+    const config = JSON.parse(
+      await readFile(path.join(ROOT, "claudia.json"), "utf8")
+    )
+
+    return get(config, key)
+  } catch (error) {
+    return null
+  }
 }
