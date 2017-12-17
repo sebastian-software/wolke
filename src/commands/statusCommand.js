a40faf98a804254468fca8bda117da136ed124df
Sebastian Fastner <s.fastner@sebastian-software.de>
2017-12-17 12:54:24 +0100

    Better status output

--- src/commands/statusCommand.js
+++ src/commands/statusCommand.js
@@ -1,22 +1,23 @@
 import chalk from "chalk"
+import get from "lodash/get"
 import {
   configurationAvailable,
   getConfiguration,
   printConfigurationErrors
 } from "../common/configuration"
 import initCommand from "./initCommand"
-import { findDistribution } from "../common/cloudFront"
 
 import {
-  cleanStages,
   getCustomDomainNames,
-  getBasePathMappings
+  getBasePathMappings,
+  getApiByName
 } from "../common/apiGateway"
-
 import {
-  findZone,
-  findDnsRecord
-} from "../common/cloudflare"
+  getVersionNumber as getLambdaVersion
+} from "../common/lambda"
+import {
+  appPkg
+} from "../common/appPackage"
 
 export default async function statusCommand(context) {
   if (!await configurationAvailable()) {
@@ -29,19 +30,48 @@ export default async function statusCommand(context) {
     return printConfigurationErrors(configuration)
   }
 
-  const zone = await findZone("wolke.run")
-  const results = await findDnsRecord(zone, "example-development.wolke.run")
-  console.log(">>>", JSON.stringify(results, null, 2))
-  return 0
-/*
-  const r2 = await getCustomDomainNames()
-  console.log(">>>", JSON.stringify(r2, null, 2))
-  const results = await getBasePathMappings("example-development.wolke.run")
-  console.log(">>>", JSON.stringify(results, null, 2))
-  return 0
-  await findDistribution()
+  const restApiId = (await getApiByName(appPkg.name)).id
 
-  console.log("STATUS", context)
+  const customDomainNames = (await getCustomDomainNames())
+    .items
+    .map((item) => ({
+      domainName: item.domainName,
+      distributionDomainName: item.distributionDomainName
+    }))
+
+  const basePathMappings = (await Promise.all(
+    customDomainNames.map(
+      (item) => getBasePathMappings(item.domainName)
+        .then((basePathMapping) => ({
+          ...item,
+          stage: get(basePathMapping, "items[0].stage"),
+          restApiId: get(basePathMapping, "items[0].restApiId")
+        }))
+    )
+  )).filter((item) => item.restApiId === restApiId)
+
+  const lambdaDeployments = await Promise.all(
+    basePathMappings.map(
+      (item) => getLambdaVersion(`wolke-${appPkg.name}`, item.stage)
+        .then((lambdaVersion) => ({
+          ...item,
+          version: lambdaVersion.FunctionVersion
+        }))
+    )
+  )
+
+  const result = lambdaDeployments.reduce((arr, item) => {
+    return {
+      ...arr,
+      [item.stage]: item
+    }
+  }, {})
+
+  console.log(`
+Current deployment status of ${appPkg.name}:
+
+
+  `)
+  console.log(">>>", JSON.stringify(result, null, 2))
   return 0
-  */
 }
