1b60c521bcef905e07b15f498dcd11b1dbbabcbb
Sebastian Fastner <s.fastner@sebastian-software.de>
2018-05-01 23:14:12 +0200

    Load wolke from process.cwd() or - if not available - from global

--- packages/wolke/src/binary.js
+++ packages/wolke/src/binary.js
@@ -1,6 +1,19 @@
-/* eslint-disable no-process-exit, promise/prefer-await-to-then, import/no-unresolved */
-import { cli } from "wolke"
+/* eslint-disable no-process-exit, promise/prefer-await-to-then, node/no-missing-require,
+                  import/no-commonjs, security/detect-non-literal-require */
 
-cli()
+let wolkePath
+
+try {
+  // Load local version of wolke if available
+  wolkePath = require.resolve("wolke", { paths: [ process.cwd() ] })
+} catch (error) {
+  // Load global version
+  wolkePath = require.resolve("wolke")
+}
+
+const wolke = require(wolkePath)
+
+wolke
+  .cli()
   .then((exitCode) => process.exit(exitCode))
   .catch((error) => console.error(error))
