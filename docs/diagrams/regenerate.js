03d9f655c4c957ae389eaef5e1d780d6d538237a
Sebastian Fastner <s.fastner@sebastian-software.de>
2018-05-10 22:51:30 +0200

    Add userRequest CSS

--- docs/diagrams/regenerate.js
+++ docs/diagrams/regenerate.js
@@ -5,6 +5,8 @@ const puppeteer = require("puppeteer")
 const path = require("path")
 const fs = require("fs")
 
+const css = require("css")
+
 async function renderGraph(definition) {
   const width = 800
   const height = 600
@@ -43,16 +45,50 @@ async function renderGraph(definition) {
     myCSS
   )
   const svg = await page.$eval("#container", (container) => container.innerHTML)
+  const svgId = await page.$eval("#container", (container) => container.childNodes[0].getAttribute("id"))
   await browser.close()
 
-  return svg.replace(/<br>/g, "<br />")
+  const svgContent = {
+    content: svg.replace(/<br>/g, "<br />"),
+    id: svgId
+  }
+
+  return svgContent
+}
+
+function fixRules(rules, id) {
+  return rules.map((item) => {
+    item.selectors = item.selectors.map((selector) => {
+      return "#" + id + " " + selector
+    })
+    return item
+  })
+}
+
+async function styleSvg(svgObject, cssContent) {
+  if (!cssContent) {
+    return svgObject.content
+  }
+
+  const svgId = svgObject.id
+  const ast = css.parse(cssContent)
+  ast.stylesheet.rules = fixRules(ast.stylesheet.rules, svgId)
+  const cssStr = css.stringify(ast)
+
+  const content = svgObject.content
+
+  const lastStylePos = content.lastIndexOf("</style>") + 8
+
+  return (
+    content.substring(0, lastStylePos) + "<style>" + cssStr + "</style>" + content.substring(lastStylePos)
+  )
 }
 
 function readFile(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, "utf8", (err, data) => {
       if (err) {
-        reject(err)
+        resolve(null)
         return
       }
 
@@ -76,11 +112,16 @@ function writeFile(path, content) {
 glob(path.join(__dirname, "./*.mmd"), async (error, files) => {
   for (const file of files) {
     console.log(file)
+    const svgFilename = file.replace(".mmd", ".svg")
+    const cssFilename = file.replace(".mmd", ".css")
+
     const graphDefinition = await readFile(file)
     const svg = await renderGraph(graphDefinition)
 
-    const svgFilename = file.replace(".mmd", ".svg")
-    await writeFile(svgFilename, svg)
+    const cssContent = await readFile(cssFilename)
+    const styledSvg = await styleSvg(svg, cssContent)
+
+    await writeFile(svgFilename, styledSvg)
     console.log(svgFilename, "written")
   }
 })
