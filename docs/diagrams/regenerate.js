5cfc814050a4abda9fb675c545836355f9592524
Sebastian Fastner <s.fastner@sebastian-software.de>
2018-05-10 23:08:49 +0200

    Use stable IDs based upon mmd file content hash

--- docs/diagrams/regenerate.js
+++ docs/diagrams/regenerate.js
@@ -4,14 +4,14 @@ const puppeteer = require("puppeteer")
 
 const path = require("path")
 const fs = require("fs")
+const crypto = require("crypto")
 
 const css = require("css")
 
-async function renderGraph(definition) {
+async function renderGraph(definition, id) {
   const width = 800
   const height = 600
   const mermaidConfig = {}
-  const myCSS = undefined
 
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
@@ -22,27 +22,16 @@ async function renderGraph(definition) {
   await page.goto(`file://${path.join(__dirname, "regenerate.html")}`)
   await page.$eval(
     "#container",
-    (container, definition, mermaidConfig, myCSS) => {
+    (container, definition, mermaidConfig, id) => {
       container.innerHTML = definition
       window.mermaid.initialize(mermaidConfig)
-
-      if (myCSS) {
-        const head = window.document.head || window.document.getElementsByTagName("head")[0]
-        const style = document.createElement("style")
-        style.type = "text/css"
-        if (style.styleSheet) {
-          style.styleSheet.cssText = myCSS
-        } else {
-          style.appendChild(document.createTextNode(myCSS))
-        }
-        head.appendChild(style)
-      }
-
-      window.mermaid.init(undefined, container)
+      window.mermaid.render(id, definition, (svgCode) => {
+        container.innerHTML = svgCode
+      })
     },
     definition,
     mermaidConfig,
-    myCSS
+    id
   )
   const svg = await page.$eval("#container", (container) => container.innerHTML)
   const svgId = await page.$eval("#container", (container) => container.childNodes[0].getAttribute("id"))
@@ -116,7 +105,14 @@ glob(path.join(__dirname, "./*.mmd"), async (error, files) => {
     const cssFilename = file.replace(".mmd", ".css")
 
     const graphDefinition = await readFile(file)
-    const svg = await renderGraph(graphDefinition)
+    const id =
+      "mmd-" +
+      crypto
+        .createHash("sha256")
+        .update(graphDefinition, "utf8")
+        .digest("hex")
+        .substring(0, 8)
+    const svg = await renderGraph(graphDefinition, id)
 
     const cssContent = await readFile(cssFilename)
     const styledSvg = await styleSvg(svg, cssContent)
